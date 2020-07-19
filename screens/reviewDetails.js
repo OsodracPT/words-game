bdc82c2e34fe603c411ee5deee3cfea1a94b2513
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-07-19 18:36:50 +0100

    Custom Card Component

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
 
 export default function ReviewDetails({ route, navigation }) {
   const item = route.params;
   return (
     <View style={globalStyles.container}>
-      <Text>{item.title}</Text>
-      <Text>{item.body}</Text>
-      <Text>{item.rating}</Text>
+      <Card>
+        <Text>{item.title}</Text>
+        <Text>{item.body}</Text>
+        <Text>{item.rating}</Text>
+      </Card>
     </View>
   );
 }
---
+++ shared/card.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function Card(props) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}></View>
+      {props.children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 6,
+    elevation: 3,
+    backgroundColor: "#fff",
+    shadowOffset: { width: 1, height: 1 },
+    shadowColor: "#333",
+    shadowOpacity: 0.3,
+    marginHorizontal: 4,
+    marginVertical: 6,
+  },
+  cardContent: {
+    marginHorizontal: 18,
+    marginVertical: 10,
+  },
+});
