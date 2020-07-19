82c6ea7c397ca28a64594aa8f3ea0460f578d995
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-07-19 18:47:13 +0100

    Using Images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import { globalStyles } from "../styles/global";
+import { StyleSheet, View, Text, Button, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
 import Card from "../shared/card";
 
 export default function ReviewDetails({ route, navigation }) {
   const item = route.params;
+  const rating = route.params.rating;
+
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{item.title}</Text>
         <Text>{item.body}</Text>
-        <Text>{item.rating}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating: </Text>
+          <Image source={images.ratings[rating]}></Image>
+        </View>
       </Card>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rating: {},
+});
